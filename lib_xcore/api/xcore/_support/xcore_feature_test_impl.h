224f61a1cf4392998a05f3f52a5f8f6691d123a3
Jack McCrea <jackmccrea@xmos.com>
2020-01-31 14:57:57 +0000

    Remove workaround for inline hack]

--- lib_xcore/api/xcore/_support/xcore_feature_test_impl.h
+++ lib_xcore/api/xcore/_support/xcore_feature_test_impl.h
@@ -1,6 +1,11 @@
 // Copyright (c) 2020, XMOS Ltd, All rights reserved
 #pragma once
 
+#include <platform.h>
+
+#if !defined(__XS1B__) && !defined(__XS1C__) && !defined(__XS2A__) && !defined(__XS3A__)
+#error Unsupported platform!
+#endif
 
 // Dual Issue
 #if defined(__XS1B__) || defined(__XS1C__)
